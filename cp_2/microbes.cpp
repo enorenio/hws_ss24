0b8560e27ded2c73e95b7f1b1b22bd489d7c4581
Aleksey <morshnev.aleksey@gmail.com>
2024-05-02 09:52:30 +0200

    Add CP 2 b & c done

---
+++ cp_2/microbes.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class SegmentTree {
+private:
+    vector<int> tree;
+    vector<int> data;
+    int n;
+
+    void build(int node, int start, int end) {
+        if (start == end) {
+            // Leaf node will have a single element
+            tree[node] = data[start] % 2;
+        } else {
+            int mid = (start + end) / 2;
+            build(2 * node + 1, start, mid);
+            build(2 * node + 2, mid + 1, end);
+            tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
+        }
+    }
+
+    void update(int idx, int value, int node, int start, int end) {
+        if (start == end) {
+            // Leaf node
+            data[idx] = value;
+            tree[node] = value % 2;
+        } else {
+            int mid = (start + end) / 2;
+            if (start <= idx && idx <= mid) {
+                update(idx, value, 2 * node + 1, start, mid);
+            } else {
+                update(idx, value, 2 * node + 2, mid + 1, end);
+            }
+            tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
+        }
+    }
+
+    int query(int l, int r, int node, int start, int end) {
+        if (r < start || end < l) {
+            // range represented by a node is completely outside the given range
+            return 0;
+        }
+        if (l <= start && end <= r) {
+            // range represented by a node is completely inside the given range
+            return tree[node];
+        }
+        int mid = (start + end) / 2;
+        int left_child = query(l, r, 2 * node + 1, start, mid);
+        int right_child = query(l, r, 2 * node + 2, mid + 1, end);
+        return left_child + right_child;
+    }
+
+public:
+    SegmentTree(vector<int>& input) {
+        data = input;
+        n = input.size();
+        tree.resize(4 * n);
+        build(0, 0, n - 1);
+    }
+
+    void update(int idx, int value) {
+        update(idx, value, 0, 0, n - 1);
+    }
+
+    int query(int l, int r) {
+        return query(l, r, 0, 0, n - 1);
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n; cin >> n;
+    int q; cin >> q;
+
+    vector<int> b(n);
+
+    for(int i = 0; i < n; i++) {
+        cin >> b[i];
+    }
+
+    SegmentTree st(b);
+
+    for(int i = 0; i < q; i++) {
+        char c; cin >> c;
+        if (c == 'U') {
+            int x, y; cin >> x >> y;
+            st.update(x - 1, y);
+        } else {
+            int x, y; cin >> x >> y;
+            cout << st.query(x - 1, y - 1) << endl;
+        }
+    }
+
+    return 0;
+}
---
+++ cp_2/theanswercount.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+vector<long long> computeSubsetSums(const vector<int>& nums) {
+    vector<long long> subsetSums = {0};
+    for (int num : nums) {
+        vector<long long> newSums;
+        for (long long sum : subsetSums) {
+            newSums.push_back(sum + num);
+        }
+        subsetSums.insert(subsetSums.end(), newSums.begin(), newSums.end());
+    }
+    return subsetSums;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n; cin >> n;
+    long long x; cin >> x;
+
+    vector<int> nums(n);
+
+    for(int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    int mid = nums.size() / 2;
+    
+    // Split the array into two halves
+    vector<int> left(nums.begin(), nums.begin() + mid);
+    vector<int> right(nums.begin() + mid, nums.end());
+    
+    // Get all subset sums for both halves
+    vector<long long> leftSums = computeSubsetSums(left);
+    vector<long long> rightSums = computeSubsetSums(right);
+    
+    // Use a hash map to count sums in the left half
+    unordered_map<long long, int> leftSumCount;
+    for (long long sum : leftSums) {
+        ++leftSumCount[sum];
+    }
+    
+    // Count the number of valid pairs
+    long long count = 0;
+    for (long long sum : rightSums) {
+        long long complement = x - sum;
+        if (leftSumCount.find(complement) != leftSumCount.end()) {
+            count += leftSumCount[complement];
+        }
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
+
+// SUBSET-SUM PROBLEM (SSP), meet-in-the-middle solution. O(2^(n/2) * n) time complexity.
